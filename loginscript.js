9fb3e95d9d63060853c32af0da705d0e34f2cf77
Mohamed Et-Tahery <met-tahe@c4r1p10.1337.ma>
2024-11-12 16:02:31 +0100

    in progress...

--- loginscript.js
+++ loginscript.js
@@ -3,61 +3,72 @@ function changemode() {
     let background = document.getElementById("signupform");
     let form_background = document.getElementById("formule");
     let subject=document.getElementById("subject");
-    
-    if (window.getComputedStyle(background).backgroundColor==="rgb(47, 10, 68)") {
-        background.style.backgroundColor="#CCC2DC";
-    } else {
-        background.style.backgroundColor="rgb(47, 10, 68)";
-    }
-
-    if (window.getComputedStyle(form_background).backgroundColor==="rgb(21, 2, 32)") {
-        form_background.style.backgroundColor="white";
-    } else {
-        form_background.style.backgroundColor="rgb(21, 2, 32)";
-    }
-
-    if (window.getComputedStyle(subject).color==="rgb(255, 255, 255)") {
-        subject.style.color="#374557";
-    } else {
-        subject.style.color="#ffffff";
-    }
-
+    let signupbtn=document.getElementById("q");
+    let darkmode=document.getElementById("darkmode-btn");
+    let signin=document.getElementById("login");
     let labels = document.getElementsByTagName("label");
+    let input_color = document.getElementsByClassName("input_color");
+
+
+    const lightMode = {
+        background: "#CCC2DC",
+        formBackground: "#ffffff",
+        subject: "#374557",
+        signupbtn_background:"#000000",
+        signupbtn_color:"#ffffff",
+        darkmode_img:"url('images/lightmode.png')",
+        signin_color:"#000000",
+        labels_color:"#364558",
+        input_color:"#000000",
+    };
+    const dakMode = {
+        background: "rgb(47, 10, 68)",
+        formBackground: "rgb(21, 2, 32)",
+        subject: "rgb(255, 255, 255)",
+        signupbtn_background:"#ffffff",
+        signupbtn_color:"#8A929C",
+        darkmode_img:"url('images/darkmode.png')",
+        signin_color:"#ffffff",
+        labels_color:"#ffffff",
+        input_color:"#ffffff",
+    };
+    const userData = {
+        background: window.getComputedStyle(background).backgroundColor,
+        formBackground: window.getComputedStyle(form_background).backgroundColor,
+        subject: window.getComputedStyle(subject).color,
+    };
+
+    if (userData.background === dakMode.background) {
+        // console.log("dark mode");
+        background.style.backgroundColor=lightMode.background;
+        form_background.style.backgroundColor=lightMode.formBackground;
+        subject.style.color = lightMode.subject;
+        signupbtn.style.backgroundColor=lightMode.signupbtn_background;
+        signupbtn.style.color=lightMode.signupbtn_color;
+        darkmode.style.backgroundImage=lightMode.darkmode_img;
+        signin.style.color=lightMode.signin_color;
+        for(let i=0;i<input_color.length;i++)
+            input_color[i].style.color=lightMode.input_color;
+    } else {
+        // console.log("light mode");
+        background.style.backgroundColor = dakMode.background;
+        form_background.style.backgroundColor= dakMode.formBackground;
+        subject.style.color= dakMode.subject;
+        signupbtn.style.backgroundColor=dakMode.signupbtn_background;
+        signupbtn.style.color=dakMode.signupbtn_color;
+        darkmode.style.backgroundImage=dakMode.darkmode_img;
+        signin.style.color=dakMode.signin_color;
+        for(let i=0;i<input_color.length;i++)
+            input_color[i].style.color=dakMode.input_color;
+    }
+    
     for (let i = 0; i < labels.length; i++) {
-        if (window.getComputedStyle(labels[i]).color==="rgb(255, 255, 255)") {
-            labels[i].style.color="#364558";
+        if (window.getComputedStyle(labels[i]).color===dakMode.labels_color) {
+            labels[i].style.color=lightMode.labels_color;
         } else {
-            labels[i].style.color="#ffffff";
+            labels[i].style.color=dakMode.labels_color;
         }
     }
 
-    let signupbtn=document.getElementById("q");
-    if (window.getComputedStyle(signupbtn).backgroundColor==="rgb(255, 255, 255)") {
-        signupbtn.style.backgroundColor="black";
-    } else {
-        signupbtn.style.backgroundColor="#ffffff";
-    }
-
-    if (window.getComputedStyle(signupbtn).color==="rgb(138, 146, 156)") {
-        signupbtn.style.color="white";
-    } else {
-        signupbtn.style.color="#8A929C";
-    }
-    let darkmode=document.getElementById("darkmode-btn");
-    if(darkmode.style.backgroundImage.includes("images/darkmode.png"))
-    {
-        darkmode.style.backgroundImage="url('images/lightmode.png')";
-    }
-    else
-    {
-        darkmode.style.backgroundImage="url('images/darkmode.png')";
-    }
-    let signin=document.getElementById("login");
-    if(window.getComputedStyle(signin).color==="rgb(255, 255, 255)")
-    {
-        signin.style.color="black";
-    }
-    else
-        signin.style.color="white";
 }
 
