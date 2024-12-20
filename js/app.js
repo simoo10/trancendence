3dbff62b19367699b353bf8a650de436942b6beb
Mohamed Et-Tahery <met-tahe@e1r2p1.1337.ma>
2024-12-20 22:46:35 +0100

    in progress...

---
+++ js/app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const myapp=document.getElementById('myapp');
+    const cssfile = document.getElementById('css-file');
+
+    const routes={
+        '/': 'home',
+        '/': 'about',
+        '/contact': 'contact'
+    }
+});
